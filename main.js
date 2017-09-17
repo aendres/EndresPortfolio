e71246be7a2c2796bca8bd7ba5836d8837520b29
Allison G. Endres <aendres19@gmail.com>
2017-09-17 19:24:35 -0400

    Add JavaScript and more CSS

---
+++ main.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var userInput = document.getElementById("userInput");
+    var myOutput = document.getElementById("myOutput");
+
+    var sentence = userInput.value;
+    myOutput.value = sentence + "!!!! ps. You're the best!"
+}
